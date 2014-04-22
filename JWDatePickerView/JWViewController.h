79d9a2267f6467dc1c9ea36bc9e2e17d62706a4e
Josh Wood <josh.m.l.wood@gmail.com>
2014-04-21 23:26:57 -0500

    Initial Commit

---
+++ JWDatePickerView/JWViewController.h
@@ -0,0 +1,13 @@
+//
+//  JWViewController.h
+//  JWDatePickerView
+//
+//  Created by Josh Wood on 4/21/14.
+//  Copyright (c) 2014 joshmlwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JWViewController : UIViewController
+
+@end
---
+++ JWDatePickerViewTests/JWDatePickerViewTests.m
@@ -0,0 +1,34 @@
+//
+//  JWDatePickerViewTests.m
+//  JWDatePickerViewTests
+//
+//  Created by Josh Wood on 4/21/14.
+//  Copyright (c) 2014 joshmlwood. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JWDatePickerViewTests : XCTestCase
+
+@end
+
+@implementation JWDatePickerViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
