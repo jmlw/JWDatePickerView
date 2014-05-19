2650c6a6ed344fbf491a4466709fb3e59f0c2218
Josh Wood <josh.m.l.wood@gmail.com>
2014-05-19 16:11:51 -0500

    Building out custom picker header

--- JWDateTimePicker/JWDateTimePicker.h
+++ JWDateTimePicker/JWDateTimePicker.h
@@ -6,8 +6,36 @@
 //  Copyright (c) 2014 joshmlwood. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface JWDateTimePicker : NSObject
+@class JWDateTimePicker;
+@protocol JWDateTimePickerProtocol <NSObject>
+
+@optional
+
+- (void) dateTimePicker:(JWDateTimePicker *)picker DidBeginScrollingFromDate:(NSDate *)startingDate;
+- (void) dateTimePicker:(JWDateTimePicker *)picker DidEndScrollingToDate:(NSDate *)endingDate;
+
+@end
+
+@interface JWDateTimePicker : UIPickerView <UIPickerViewDelegate, UIPickerViewDataSource>
+
+typedef enum{
+    JWDateTimePickerModeTime,
+    JWDateTimePickerModeDate,
+    JWDateTimePickerModeDateAndTime
+} JWDateTimePickerMode;
+
+@property (nonatomic,copy)      NSCalendar              * calendar;
+@property (nonatomic,retain)    NSDate                  * date;
+@property (nonatomic,retain)    NSLocale                * locale;
+@property (nonatomic,retain)    NSTimeZone              * timezone;
+@property (nonatomic)           JWDateTimePickerMode    dateTimePickerMode;
+@property (nonatomic,retain)    NSDate                  * minimumDate;
+@property (nonatomic,retain)    NSDate                  * maximumDate;
+@property (nonatomic)           NSInteger               * minuteInterval;
+
+- (void) setDate:(NSDate *)date animated:(BOOL)animated;
+- (NSDate *) selectedDate;
 
 @end
